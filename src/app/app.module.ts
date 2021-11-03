ba49bb2df01a253683a66d820c376b9a3835f81c
Leandro Vicentin <leandrovicentin94@gmail.com>
2021-11-03 19:52:55 -0300

    Finalizando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { CourseModule } from './courses/course.module';
-import { Error404Component } from './error-404/error-404.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
